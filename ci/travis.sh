891bf494c13211f456856d461a5496450b5131e3
Anton Tiurin <noxiouz@yandex.ru>
2015-02-27 16:28:44 +0300

    [Travis] Let's go!

---
+++ ci/travis.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+install_utility () {
+    sudo apt-get update -qq && sudo apt-get install -qq devscripts build-essential equivs python-software-properties
+}
+
+build_handystats () {
+  git clone --recursive https://github.com/hindo/handystats.git -b master
+  cd cocaine-core
+  yes | sudo mk-build-deps -i
+  yes | debuild -uc -us
+  cd .. && sudo dpkg -i *.deb || sudo apt-get install -f && rm -rf handystats
+}
+
+make_env () {
+    echo "Install utility packages..."
+    install_utility
+    echo "Build & install packages..."
+    build_handystats
+    echo "Waiting..."
+    sleep 1
+}
+
+make_env
